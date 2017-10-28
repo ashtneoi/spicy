919f112a786281318a6d8b0c4584dc79c14e2068
drkitty <bumblingkitty@gmail.com>
2017-10-28 14:30:10 -0700

    Test memory initialization, sort of

--- test.py
+++ test.py
@@ -4,9 +4,17 @@
 import spicy
 
 
-spicy.Emulator(spicy.PIC16F1454(), (
-    # ssXXXXssssXXXX
-    0b11000001111110,  # movlw 0x3E
-    0b00011011110000,  # xorwf 0x70
-    0b11001111111110,  # bra -2 (0x1FE)
-)).run()
+def test_init():
+    d = spicy.PIC16F1454()
+    d.por()
+    print(' '.join('{:02X}'.format(x) for x in d.datamem[0x00:0x0C]))
+
+def test_simple_loop():
+    spicy.Emulator(spicy.PIC16F1454(), (
+        # ssXXXXssssXXXX
+        0b11000001111110,  # movlw 0x3E
+        0b00011011110000,  # xorwf 0x70
+        0b11001111111110,  # bra -2 (0x1FE)
+    )).run()
+
+test_init()
